12a8b9a2e44ab843fb786febafbf283257f427eb
Çağrı Aydın <cagriaydin@Cagrs-MacBook-Pro.local>
2020-04-24 12:13:10 +0300

    android height fix

--- lib/pages/health.dart
+++ lib/pages/health.dart
@@ -35,11 +35,11 @@ class HealthStatus extends StatelessWidget {
               ),
             ),
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
                     height: size.height * .7,
                     child: HealthWidgetController(
                       healthStatusModelList: [
@@ -54,49 +54,49 @@ class HealthStatus extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Center(
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: size.width - 100,
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.circular(21.5),
-                          boxShadow: [
-                            CustomBoxShadow(
-                                color: Theme.of(context).primaryColorDark,
-                                offset: new Offset(3, 3),
-                                blurRadius: 2.0,
-                                blurStyle: BlurStyle.outer),
-                            CustomBoxShadow(
-                                color: Theme.of(context).primaryColorDark,
-                                offset: new Offset(-1, 3),
-                                blurRadius: 2.0,
-                                blurStyle: BlurStyle.outer),
-                            CustomBoxShadow(
-                                color: Colors.white,
-                                offset: new Offset(1, -0.2),
-                                blurRadius: 5.0,
-                                blurStyle: BlurStyle.outer),
-                          ],
-                        ),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "Durum Bildir",
-                              style: TextStyle(
-                                color: Color(0xFF26315F),
-                                fontSize: 20,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                          ),
-                        ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: size.width - 100,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(21.5),
+                  boxShadow: [
+                    CustomBoxShadow(
+                        color: Theme.of(context).primaryColorDark,
+                        offset: new Offset(3, 3),
+                        blurRadius: 2.0,
+                        blurStyle: BlurStyle.outer),
+                    CustomBoxShadow(
+                        color: Theme.of(context).primaryColorDark,
+                        offset: new Offset(-1, 3),
+                        blurRadius: 2.0,
+                        blurStyle: BlurStyle.outer),
+                    CustomBoxShadow(
+                        color: Colors.white,
+                        offset: new Offset(1, -0.2),
+                        blurRadius: 5.0,
+                        blurStyle: BlurStyle.outer),
+                  ],
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Durum Bildir",
+                      style: TextStyle(
+                        color: Color(0xFF26315F),
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
                       ),
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ),
