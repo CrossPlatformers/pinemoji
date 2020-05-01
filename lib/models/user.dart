7ff1c30c245eb24fa8ad8a4cd1bc9f3fa6914e85
Çağrı Aydın <cagriaydin@Cagrs-MacBook-Pro.local>
2020-05-01 10:59:26 +0300

    user model fixed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -10,83 +10,69 @@ User userFromJson(String str) => User.fromMap(json.decode(str));
 String userToJson(User data) => json.encode(data.toMap());
 
 class User {
-    final String id;
-    final String name;
-    final String surname;
-    final ExtraInfo extraInfo;
-    final String model;
-    final String brand;
-    final String os;
-    final String phoneNumber;
+  final String id;
+  final String name;
+  final String surname;
+  final Map extraInfo;
+  final String model;
+  final String brand;
+  final String os;
+  final String phoneNumber;
 
-    User({
-        @required this.id,
-        @required this.name,
-        @required this.surname,
-        @required this.extraInfo,
-        @required this.model,
-        @required this.brand,
-        @required this.os,
-        @required this.phoneNumber,
-    });
+  User({
+    @required this.id,
+    @required this.name,
+    @required this.surname,
+    @required this.extraInfo,
+    @required this.model,
+    @required this.brand,
+    @required this.os,
+    @required this.phoneNumber,
+  });
 
-    User copyWith({
-        String id,
-        String name,
-        String surname,
-        ExtraInfo extraInfo,
-        String model,
-        String brand,
-        String os,
-        String phoneNumber,
-    }) => 
-        User(
-            id: id ?? this.id,
-            name: name ?? this.name,
-            surname: surname ?? this.surname,
-            extraInfo: extraInfo ?? this.extraInfo,
-            model: model ?? this.model,
-            brand: brand ?? this.brand,
-            os: os ?? this.os,
-            phoneNumber: phoneNumber ?? this.phoneNumber,
-        );
+  User copyWith({
+    String id,
+    String name,
+    String surname,
+    Map extraInfo,
+    String model,
+    String brand,
+    String os,
+    String phoneNumber,
+  }) =>
+      User(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        surname: surname ?? this.surname,
+        extraInfo: extraInfo ?? this.extraInfo,
+        model: model ?? this.model,
+        brand: brand ?? this.brand,
+        os: os ?? this.os,
+        phoneNumber: phoneNumber ?? this.phoneNumber,
+      );
 
-    factory User.fromMap(Map<String, dynamic> json) => User(
+  factory User.fromMap(Map<String, dynamic> json) => User(
         id: json["id"] == null ? null : json["id"],
         name: json["name"] == null ? null : json["name"],
         surname: json["surname"] == null ? null : json["surname"],
-        extraInfo: json["extraInfo"] == null ? null : ExtraInfo.fromMap(json["extraInfo"]),
+        extraInfo:
+            json["extraInfo"] == null ? null : Map.from(json["extraInfo"]),
         model: json["model"] == null ? null : json["model"],
         brand: json["brand"] == null ? null : json["brand"],
         os: json["os"] == null ? null : json["os"],
         phoneNumber: json["phoneNumber"] == null ? null : json["phoneNumber"],
-    );
+      );
 
-    Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() => {
         "id": id == null ? null : id,
         "name": name == null ? null : name,
         "surname": surname == null ? null : surname,
-        "extraInfo": extraInfo == null ? null : extraInfo.toMap(),
+        "extraInfo": extraInfo == null ? null : extraInfo,
         "model": model == null ? null : model,
         "brand": brand == null ? null : brand,
         "os": os == null ? null : os,
         "phoneNumber": phoneNumber == null ? null : phoneNumber,
-    };
-}
-
-class ExtraInfo {
-    ExtraInfo();
-
-    ExtraInfo copyWith({
-    }) => 
-        ExtraInfo(
-        );
-
-    factory ExtraInfo.fromMap(Map<String, dynamic> json) => ExtraInfo(
-    );
-
-    Map<String, dynamic> toMap() => {
-    };
+      };
 }
 
 //{
