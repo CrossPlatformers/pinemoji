c9c80a557923ae8a2fd85454cb30b621b23d835d
onatcipli <onatcipli@outlook.com>
2020-05-03 12:51:02 +0300

    map update

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:pinemoji/pages/bottom-navigation.dart';
 import 'package:pinemoji/pages/welcome.dart';
 import 'package:pinemoji/shared/custom_theme.dart';
 
 void main() => runApp(
-      MyApp(),
-      // DevicePreview(
-      //   builder: (context) => MyApp(),
-      //   enabled: true,
-      // ),
+      DevicePreview(
+        builder: (context) => MyApp(),
+        enabled: false,
+      ),
     );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.of(context).locale,
+      // <--- Add the locale
+      builder: DevicePreview.appBuilder,
+      // <--- Add the builder
+      debugShowCheckedModeBanner: false,
       theme: customTheme,
       home: WelcomePage(),
     );
