0230e60ee8d348a7318d6844ab2e61b2151459ba
Mustafa Berkay <Mustafa.Berkay@logo.com.tr>
2020-05-06 02:44:21 +0300

    font-size fix

--- lib/widgets/survey-card.dart
+++ lib/widgets/survey-card.dart
@@ -75,7 +75,7 @@ class SurveyCard extends StatelessWidget {
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         color: Theme.of(context).primaryColorLight,
-                        fontSize: 14),
+                        fontSize: 12),
                   ),
                 ],
               )),
