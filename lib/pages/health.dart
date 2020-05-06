9cc4ffa81f7684f1ecd5ada5ffbb49eb7692ecbd
Çağrı Aydın <cagriaydin@Cagrs-MacBook-Pro.local>
2020-05-06 03:30:18 +0300

    circular progress indicators and snackbars

--- lib/pages/health.dart
+++ lib/pages/health.dart
@@ -21,37 +21,86 @@ class _HealthStatusState extends State<HealthStatus> {
   Map<String, String> resultMap;
   @override
   void initState() {
-    // TODO: implement initState
-    SurveyRepository().getOwnData(HealthStatus.survey.id).then((map) => setState(() => resultMap = map));
+    SurveyRepository()
+        .getOwnData(HealthStatus.survey.id)
+        .then((map) => setState(() => resultMap = map));
     super.initState();
   }
 
+  final _scaffOldState = GlobalKey<ScaffoldState>();
+  bool hasLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          StatusTitle(
-            HealthStatus.survey.info,
-            180,
-          ),
-          VerticalSlider(
-            survey: HealthStatus.survey,
-            result: resultMap,
-          ),
-          OutcomeButton(
-            text: "Durum Bildir",
-            action: () {
-              SurveyRepository().sendSurvey(HealthStatus.survey.id, resultMap).then((onValue) => {
-                    //TODO: show response
-                  });
-            },
-          ),
-          SizedBox(
-            height: 40,
-          ),
-        ],
+    return Scaffold(
+      key: _scaffOldState,
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            StatusTitle(
+              HealthStatus.survey.info,
+              180,
+            ),
+            hasLoading
+                ? Expanded(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircularProgressIndicator(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Anket Gönderiliyor...",
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                : VerticalSlider(
+                    survey: HealthStatus.survey,
+                    result: resultMap,
+                  ),
+            !hasLoading
+                ? OutcomeButton(
+                    text: "Durum Bildir",
+                    action: () async {
+                      setState(() {
+                        hasLoading = true;
+                      });
+                      var status = await SurveyRepository()
+                          .sendSurvey(HealthStatus.survey.id, resultMap);
+                      setState(() {
+                        hasLoading = false;
+                      });
+                      if (status) {
+                        showWarning("Anket başarıyla gönderildi");
+                      } else {
+                        showWarning(
+                            "İşlem sırasında hata oluştu, lütfen tekrar deneyiniz");
+                      }
+                    },
+                  )
+                : Container(),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showWarning(String text) {
+    _scaffOldState.currentState.showSnackBar(
+      SnackBar(
+        content: Text(text),
       ),
     );
   }
